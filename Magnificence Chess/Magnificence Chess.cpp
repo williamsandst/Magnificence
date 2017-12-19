4b1c92a45858339875c8fd5f431d90adb24c717d
William Sandström <wilsan@elevad.umea.se>
2017-12-19 13:32:48 +0100

    Changed language to c++

---
+++ Magnificence Chess/Magnificence Chess.cpp
@@ -0,0 +1,16 @@
+// Magnificence Chess.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char x;
+	cout << "Hello World";
+	cin >> x;
+    return 0;
+}
+
